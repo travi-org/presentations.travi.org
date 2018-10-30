4b27b049097cb0292480a455b28dc8b26d8b4ba0
Matt Travi <programmer@travi.org>
2018-10-29 23:44:15 -0500

    feat(theme): wrapped the site in the travi.org theme

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,32 +2,38 @@ import React, {Fragment} from 'react';
 import {node} from 'prop-types';
 import Helmet from 'react-helmet';
 import {CssBaseline} from '@material-ui/core';
+import {MuiThemeProvider} from '@material-ui/core/styles';
 import {graphql, StaticQuery} from 'gatsby';
+import {Layout as LayoutComponent, theme} from '@travi/matt.travi.org-components';
 
 export default function Layout({children}) {
   /* eslint-disable react/jsx-no-bind */
   return (
-    <StaticQuery
-      query={graphql`
-        query LayoutQuery {
-          site {
-            siteMetadata {
-              title
+    <MuiThemeProvider theme={theme}>
+      <LayoutComponent>
+        <StaticQuery
+          query={graphql`
+            query LayoutQuery {
+              site {
+                siteMetadata {
+                  title
+                }
+              }
             }
-          }
-        }
-      `}
-      render={data => (
-        <Fragment>
-          <Helmet
-            titleTemplate={`%s | ${data.site.siteMetadata.title}`}
-            defaultTitle={data.site.siteMetadata.title}
-          />
-          <CssBaseline />
-          {children}
-        </Fragment>
-      )}
-    />
+          `}
+          render={data => (
+            <Fragment>
+              <Helmet
+                titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+                defaultTitle={data.site.siteMetadata.title}
+              />
+              <CssBaseline />
+              {children}
+            </Fragment>
+          )}
+        />
+      </LayoutComponent>
+    </MuiThemeProvider>
   );
   /* eslint-enable react/jsx-no-bind */
 }
