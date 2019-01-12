06ac267d57551593b2de0b91f44b9dd751c7d6a5
Matt Travi <programmer@travi.org>
2019-01-11 20:56:56 -0600

    refactor(theme): stopped double theming
    since the layout component now handles it

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,14 @@
 import React, {Fragment} from 'react';
 import {node} from 'prop-types';
 import Helmet from 'react-helmet';
-import {CssBaseline} from '@material-ui/core';
-import {MuiThemeProvider} from '@material-ui/core/styles';
 import {graphql, StaticQuery} from 'gatsby';
-import {Layout as LayoutComponent, theme} from '@travi/matt.travi.org-components';
+import {Layout as LayoutComponent} from '@travi/matt.travi.org-components';
 
 export default function Layout({children}) {
-  /* eslint-disable react/jsx-no-bind */
   return (
-    <MuiThemeProvider theme={theme}>
-      <LayoutComponent>
-        <StaticQuery
-          query={graphql`
+    <LayoutComponent>
+      <StaticQuery
+        query={graphql`
             query LayoutQuery {
               site {
                 siteMetadata {
@@ -21,21 +17,18 @@ export default function Layout({children}) {
               }
             }
           `}
-          render={data => (
-            <Fragment>
-              <Helmet
-                titleTemplate={`%s | ${data.site.siteMetadata.title}`}
-                defaultTitle={data.site.siteMetadata.title}
-              />
-              <CssBaseline />
-              {children}
-            </Fragment>
-          )}
-        />
-      </LayoutComponent>
-    </MuiThemeProvider>
+        render={data => (
+          <Fragment>
+            <Helmet
+              titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+              defaultTitle={data.site.siteMetadata.title}
+            />
+            {children}
+          </Fragment>
+        )}
+      />
+    </LayoutComponent>
   );
-  /* eslint-enable react/jsx-no-bind */
 }
 
 Layout.propTypes = {
